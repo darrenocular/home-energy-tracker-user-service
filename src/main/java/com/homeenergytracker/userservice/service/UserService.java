b58dd3086511056c438258b5341f3a7d6aa84ab3
Darren Leong <darrenljj07@hotmail.com>
2026-07-31 00:05:52 +0800

    feat: Add logging and execution time aspects

--- src/main/java/com/homeenergytracker/userservice/service/UserService.java
+++ src/main/java/com/homeenergytracker/userservice/service/UserService.java
@@ -16,8 +16,6 @@ public class UserService {
     }
 
     public UserDto createUser(UserDto input) {
-        log.info("Creating user {}", input);
-
         final User createdUser = User.builder()
                 .firstName(input.getFirstName())
                 .lastName(input.getLastName())
@@ -32,15 +30,12 @@ public class UserService {
     }
 
     public UserDto getUserById(Long id) {
-        log.info("Retrieving user by id {}", id);
         return userRepository.findById(id)
                 .map(this::toDto)
                 .orElse(null);
     }
 
     public void updateUser(Long id, UserDto input) {
-        log.info("Updating user {}", input);
-
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
 
@@ -54,7 +49,6 @@ public class UserService {
     }
 
     public void deleteUser(Long id) {
-        log.info("Deleting user {}", id);
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         userRepository.delete(user);
