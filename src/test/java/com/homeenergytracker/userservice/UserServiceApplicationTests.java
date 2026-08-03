8cd7f615b1ad39e2bbe5f9e5c295f0193bd8d92e
Darren Leong <darrenljj07@hotmail.com>
2026-08-03 16:17:10 +0800

    feat: Add users test data

--- src/test/java/com/homeenergytracker/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/homeenergytracker/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package com.homeenergytracker.userservice;
 
+import com.homeenergytracker.userservice.entity.User;
+import com.homeenergytracker.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class UserServiceApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final int NUMBER_OF_USERS = 10;
 
     @Test
     void contextLoads() {
     }
 
+    @Disabled
+    @Test
+    void addUsersToDB() {
+        for (int i = 1; i <= NUMBER_OF_USERS; i++) {
+            User user = User.builder()
+                    .firstName("User " + i)
+                    .lastName("LastName" + i)
+                    .email("user" + i + "@example.com")
+                    .address(i + "Example St")
+                    .alerting(i % 2 == 0)
+                    .energyAlertingThreshold(1000.0 + i)
+                    .build();
+            userRepository.save(user);
+        }
+        log.info("User Repository has been populated");
+    }
 }
